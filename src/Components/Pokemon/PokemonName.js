250681ae607a2318d6fbf92c40131947f6d3af2d
Shaun Yap <me@shaunyap.ca>
2021-02-20 22:38:31 -0800

    Image pagination button css

--- src/Components/Pokemon/PokemonName.js
+++ src/Components/Pokemon/PokemonName.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './Pokemon.scss';
+
 export default function PokemonName(props) {
   const {selectedPokemon} = props;
 
   return (
-    <h3>{selectedPokemon.name}</h3>
+    <h3 className="pokemon-name">{selectedPokemon.name}</h3>
   )
 }
