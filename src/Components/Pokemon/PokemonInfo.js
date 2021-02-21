053dad19dd10125fc5b346e7a0cc540ed1b67df4
Shaun Yap <me@shaunyap.ca>
2021-02-21 14:22:55 -0800

    Stats css styling

--- src/Components/Pokemon/PokemonInfo.js
+++ src/Components/Pokemon/PokemonInfo.js
@@ -11,19 +11,22 @@ export default function PokemonInfo(props) {
 
   const typeFormat = selectedPokemon.types.map((type, index) => {
     return (
-      <li key={type.slot}>{type.type.name}</li>
+      <li key={type.slot} className="type-item">{type.type.name}</li>
     )
   })
 
   const abilityFormat = selectedPokemon.abilities.map(ability => {
     return (
-      <li key={ability.slot}>{ability.ability.name}</li>
+      <li key={ability.slot} className="ability-item">{ability.ability.name}</li>
     )
   })
 
   const statsFormat = selectedPokemon.stats.map(stat => {
     return (
-      <li key={stat.stat.name}>{stat.stat.name}: {stat.base_stat}</li>
+      <div key={stat.stat.name} className="stats-item">
+        <li>{stat.stat.name}: </li>
+        <p>{stat.base_stat}</p>
+      </div>
     )
   })
 
@@ -32,20 +35,25 @@ export default function PokemonInfo(props) {
       <div className="selected-pokemon-type">
         {/* Plurality type check */}
         <p>{ selectedPokemon.types.length > 1 ? 'Types: '  : `Type: ${selectedPokemon.types[0].type.name}` }</p>
-        { selectedPokemon.types.length > 1 ? <ul>{typeFormat}</ul>  : "" }
+        { selectedPokemon.types.length > 1 ? <ul className="pokemon-info types-list">{typeFormat}</ul>  : "" }
       </div>
       <div className="selected-pokemon-ability">
         <p>Abilities:</p>
-        <ul>
+        <ul className="pokemon-info abilities-list">
           {abilityFormat}
         </ul>
       </div>
-      <p>Height: {selectedPokemon.height}</p>
-      <p>Weight: {selectedPokemon.weight}</p>
-    
+      <div className="pokemon-info-height-weight">
+        <label>Height: </label>
+        <p>{selectedPokemon.height}</p>
+      </div>
+      <div className="pokemon-info-height-weight">
+        <label>Weight: </label>
+        <p>{selectedPokemon.weight}</p>
+      </div>    
       <div className="selected-pokemon-stats">
         <p>Base Stats:</p>
-        <ul>
+        <ul className="pokemon-info stats-list">
           {statsFormat}
         </ul>
       </div>
